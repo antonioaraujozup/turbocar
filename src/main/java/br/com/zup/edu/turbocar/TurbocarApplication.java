d637c8eda540d6ef5aa6b29056d986d2c457863c
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-26 16:41:32 -0300

    Commit inicial

---
+++ src/main/java/br/com/zup/edu/turbocar/TurbocarApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.turbocar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbocarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbocarApplication.class, args);
+	}
+
+}
